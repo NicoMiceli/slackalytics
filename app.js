787d591df1b90b165d86368a26d7e421f1e071e8
Nico Miceli <nico@Nicos-MacBook-Pro.local>
2015-04-04 00:32:10 -0400

    more features

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var qs = require('querystring');
+var channel = {
+	id: 	req.body.channel_id,
+	name: 	req.body.channel_name
+	}
+var user = {
+	id: 	req.body.user_id,
+	name: 	req.body.user_name
+}
+var msgText = req.body.text
 
 //Server Details
 var app = express();
@@ -22,7 +31,7 @@ app.get('/', function(req, res){
 
 app.post('/collect', function(req, res){	
 
-//Structure Data
+	//Structure Data
 	var data = {
 		v: 		1,
 		tid: 	"UA-61435895-1",
@@ -33,17 +42,15 @@ app.post('/collect', function(req, res){
 		t: 		"event",
 		ec: 	"slack: "+ req.body.channel_name + " " + req.body.channel_id,
 		ea: 	"post by " + req.body.user_name + "|"+req.body.user_id,
-		el: 	req.body.text,
+		el: 	msgText,
 		ev: 	300 	
 	}
-
-//Make Post Request	
+	console.log(req.body.channel_name+"|"+user.name+"|"+msgText)
+	//Make Post Request	
 	request.post("https://www.google-analytics.com/collect?"  + qs.stringify(data), 
 		function(error, resp, body){
 		console.log(error);
 	})
-	
-
 });
 
 
