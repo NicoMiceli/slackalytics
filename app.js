a1749fb0296b4e17bbbf2a4db2d16b2c450e6a42
Lee Isensee <leeisensee@gmail.com>
2015-09-13 16:21:47 -0700

    add Mixpanel, switch to ENV VARs, and split up debug logging

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+//Set up Env Vars
+var env_var = {
+	debug: process.env.DEBUG,  // supports: info and debug
+	ga_key: process.env.GOOGLE_ANALYTICS_PROD,
+	mixpanel_key: process.env.MIXPANEL_PROD
+};
+
 //Set up Reqs
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var qs = require('querystring');
-
+var math = require('mathjs');
+var moment = require('moment');
 
 //Server Details
 var app = express();
@@ -14,22 +22,41 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
+//Simple Base64 handler
+var base64 = exports;
+base64.encode = function (unencoded) {
+	return new Buffer(unencoded || '').toString('base64');
+};
+
+base64.decode = function (encoded) {
+	return new Buffer(encoded || '', 'base64').toString('utf8');
+};
+
 
 //Routes
 app.get('/', function(req, res){
 	res.send('here');
 });
 
-app.post('/collect', function(req, res){
+app.get('/ping', function(req, res){
+	res.send('I\'m alive!');
+});
 
+app.post('/collect', function(req, res){
 
 	var channel = {
 		id: 	req.body.channel_id,
 		name: 	req.body.channel_name
 	};
+
 	var user = {
-		id: 	req.body.user_id
+		id: 	req.body.user_id,
+		name:	req.body.user_name
 	};
+
+	var teamDomain = req.body.team_domain;
+
+	var msgTime = math.round(req.body.timestamp, 0); //in epoch
 	
 	var msgText = req.body.text;
 
@@ -55,36 +82,89 @@ app.post('/collect', function(req, res){
 	var questionMark = searchM(/\?/g);
 	var elipseCount = searchM(/\.\.\./g);
 
-
 	//Structure Data
-	var data = {
+	var GAdata = {
 		v: 		1,
-		tid: 	"UA-XXXXXXXX-1",
+		tid: 	env_var.ga_key,
 		cid: 	user.id,
-		ds:  	"slack", //data source
-		cs: 	"slack", // campaign source
-		cd1: 	user.id,
-		cd2: 	channel.name,
+		uid: 	user.name,
+		ds:  	"slack",
+		cd1: 	user.name + " (" + user.id + ")",
+		cd2: 	channel.name + " (" + channel.id + ")",
 		cd3: 	msgText,
 		cm1: 	wordCount,
 		cm2: 	emojiCount,
 		cm3: 	exclaCount,
-	//	cm4: 	letterCount,
+		//cm4: 	letterCount,
 		cm5: 	elipseCount, 
-		cm6: 	questionMark, //need to set up in GA
-		t: 	"event",
-		ec: 	"slack: "+ channel.name + "|" + channel.id,
-		ea: 	"post by " + user.id,
+		cm6: 	questionMark,
+		dh:		teamDomain+".slack.com",
+		dp:		"/"+channel.name,
+		dt:		"Slack Channel: "+channel.name,
+		t: 		"event",
+		ec: 	"slack: "+ channel.name + " | " + channel.id,
+		ea: 	"message posted",
 		el: 	msgText,
 		ev: 	1 
 	};
-	console.log(JSON.stringify(data));
-	console.log(req.body);
-	//Make Post Request	
-	request.post("https://www.google-analytics.com/collect?" + qs.stringify(data), 
-		function(error, resp, body){
-		console.log(error);
-	})
+	
+	var mixTrack = {
+		event:	"message posted",
+		properties:	{
+				$os: "Slack",
+				$browser: "Slack",
+				$current_url: "https://"+teamDomain+".slack.com/" + channel.name,
+				mp_lib: "slack_nodeJS",
+				$lib_version: "Slack 0.0.1",
+				distinct_id: user.id,
+				num_words: wordCount,
+				num_emoji: emojiCount,
+				num_exclamation: exclaCount,
+				num_ellipsis: elipseCount,
+				num_questions: questionMark,
+				message: msgText,
+				channel: channel.name + " (" + channel.id + ")",
+				user: user.name + " (" + user.id + ")",
+				time: msgTime,
+				token: env_var.mixpanel_key
+			}
+	};
+
+	var engage_channel_info = {};
+	engage_channel_info[channel.name] = 1;
+	engage_channel_info["total_posts"] = 1;
+
+	var mixEngage = {
+		$distinct_id: user.id,
+		$time: msgTime,
+		$token: env_var.mixpanel_key,
+		$add: engage_channel_info,
+		$set: {
+				last_post: moment.unix(msgTime).format('YYYY-MM-DDThh:mm:ss')
+		}		
+	};
+
+	
+	if (env_var.debug.toLowerCase() === "info") {
+	console.log("Raw Slack Webhook Post: "+JSON.stringify(req.body));
+	console.log("Google Analytics Data: "+JSON.stringify(GAdata));
+	console.log("Mixpanel Tracking Data: "+JSON.stringify(mixTrack));
+	console.log("Mixpanel Engage Data: "+JSON.stringify(mixEngage));
+	}
+	
+	if (env_var.debug.toLowerCase() === "debug") {
+	console.log("Raw Slack Webhook Post: "+JSON.stringify(req.body));
+	console.log("Google Analytics Data: "+JSON.stringify(GAdata));
+	console.log("Mixpanel Tracking Data: "+JSON.stringify(mixTrack));
+	console.log("Mixpanel Engage Data: "+JSON.stringify(mixEngage));
+	console.log("Google Analytics Tracking Post Output: https://www.google-analytics.com/collect?" + qs.stringify(GAdata));
+	console.log("Mixpanel Tracking Post Output: https://api.mixpanel.com/track/?data=" + encodeURIComponent(base64.encode(JSON.stringify(mixTrack))) + "&ip=0");
+	console.log("Mixpanel Engage Post Output: https://api.mixpanel.com/engage/?data=" + encodeURIComponent(base64.encode(JSON.stringify(mixEngage))) + "&ip=0");
+	}
+	//Make Post Requests
+	request.post("https://www.google-analytics.com/collect?" + qs.stringify(GAdata), function(error, resp, body){console.log(error);});
+	request.post("https://api.mixpanel.com/track/?data=" + encodeURIComponent(base64.encode(JSON.stringify(mixTrack))) + "&ip=0", function(error, resp, body){console.log(error);});
+	request.post("https://api.mixpanel.com/engage/?data=" + encodeURIComponent(base64.encode(JSON.stringify(mixEngage))) + "&ip=0", function(error, resp, body){console.log(error);});
 });
 
 //Start Server
