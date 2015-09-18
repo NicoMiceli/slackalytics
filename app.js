6adba639b96b8ac07f764c97fe02ea226f3da4c2
Nico Miceli <nico@Nicos-MacBook-Pro.local>
2015-09-17 21:04:28 -0400

    add env vars for ga id

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var qs = require('querystring');
 
+//set up heroku environment variables
+var env_var = {
+	ga_key: process.env.GOOGLE_ANALYTICS_UAID
+};
 
 //Server Details
 var app = express();
@@ -22,7 +26,6 @@ app.get('/', function(req, res){
 
 app.post('/collect', function(req, res){
 
-
 	var channel = {
 		id: 	req.body.channel_id,
 		name: 	req.body.channel_name
@@ -30,8 +33,9 @@ app.post('/collect', function(req, res){
 	var user = {
 		id: 	req.body.user_id
 	};
-	
 	var msgText = req.body.text;
+	var teamDomain = req.body.team_domain;
+
 
 	function searchM(regex){
 		var searchStr = msgText.match(regex);
@@ -49,16 +53,18 @@ app.post('/collect', function(req, res){
 		return 0;
 	};
 
+
 	var wordCount = searchS(/\s+\b/);
 	var emojiCount = searchM(/:[a-z_0-9]*:/g);
 	var exclaCount = searchM(/!/g);
 	var questionMark = searchM(/\?/g);
 	var elipseCount = searchM(/\.\.\./g);
 
+
 	//Structure Data
 	var data = {
 		v: 		1,
-		tid: 	"UA-61435895-1",
+		tid: 	env_var.ga_key,
 		cid: 	user.id,
 		ds:  	"slack", //data source
 		cs: 	"slack", // campaign source
@@ -71,6 +77,9 @@ app.post('/collect', function(req, res){
 	//	cm4: 	letterCount,
 		cm5: 	elipseCount, 
 		cm6: 	questionMark, //need to set up in GA
+		dh:		teamDomain+".slack.com",
+		dp:		"/"+channel.name,
+		dt:		"Slack Channel: "+channel.name,
 		t: 		"event",
 		ec: 	"slack: "+ channel.name + "|" + channel.id,
 		ea: 	"post by " + user.id,
